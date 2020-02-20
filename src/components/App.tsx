e60961ec02b1afb0be3fb10b45815b5e108c3342
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-02-20 13:57:19 +0100

    setting up ml5 compatibility and img clissification

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import List from './list'
+import Dropzone from './Dropzone'
+
+function App() {
+  return (
+    <div className="App">
+      <Dropzone/>
+      <List/>
+    </div>
+  );
+}
+
+export default App;
